9ff58c7e263d2a19cbdfc89c57cc4530814c0fc2
akahigeg <akahigeg@gmail.com>
2015-04-14 07:58:07 +0900

    init

---
+++ concrete5/application/config/generated_overrides/concrete.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * -----------------------------------------------------------------------------
+ * Generated 2015-04-14T07:32:28+09:00
+ *
+ * @item      misc.seen_introduction
+ * @group     concrete
+ * @namespace null
+ * -----------------------------------------------------------------------------
+ */
+return array(
+    'locale' => 'ja_JP',
+    'site' => 'デモ',
+    'version_installed' => '5.7.3.1',
+    'misc' => array(
+        'access_entity_updated' => 1428964284,
+        'latest_version' => '5.7.3.1',
+        'seen_introduction' => true
+    )
+);
---
+++ concrete5/application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/b42c907e07383d4d/e9ef49ecc714aa8c.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_ee66cb10d0892927da4950432a1bb0b2_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1429350445;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1428964341;
